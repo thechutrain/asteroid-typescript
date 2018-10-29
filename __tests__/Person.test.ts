c0b540454f2259d768f2b234b719fbf3f83ffa92
thechutrain <chu.chuyalan@gmail.com>
2018-10-29 17:49:00 -0400

    index file linked to html, d prev testings files

--- __tests__/Person.test.ts
+++ __tests__/Person.test.ts
@@ -1,4 +1,4 @@
-import Person, { randomAge } from '../src/Person';
+import Person, { randomAge } from '../src/scripts/Person';
 
 describe('Person test', () => {
 	it('should be able to create a person', () => {
--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import Person, { randomAge } from './Person';
-
-const bob = new Person('bob', randomAge());
-bob.describeMe();
