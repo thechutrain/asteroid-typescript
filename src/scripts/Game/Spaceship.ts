ea51dd84253b881550855be5b6020b4efe574aaa
thechutrain <chu.chuyalan@gmail.com>
2018-10-31 12:43:43 -0400

    starting point for spaceship class thats extending DrawableClass

--- src/scripts/Game/Spaceship.ts
+++ src/scripts/Game/Spaceship.ts
@@ -1,3 +1,16 @@
 import DrawableClass from './DrawableClass';
 
-class Spaceship extends DrawableClass {}
+class Spaceship extends DrawableClass {
+	getInitOrigin(options: any): PointModel {
+		throw new Error('Method not implemented.');
+	}
+	getInitVelocity(options: any): VelocityModel {
+		throw new Error('Method not implemented.');
+	}
+	public calcPoints(ticks: number): PointModel[] {
+		throw new Error('Method not implemented.');
+	}
+	public drawPoints(): void {
+		throw new Error('Method not implemented.');
+	}
+}
